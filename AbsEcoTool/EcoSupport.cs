0a74f3d68c1438c686f94d2db5a1cae67929e259
Kalle Launiala <kalle.launiala@protonit.net>
2012-02-02 05:51:35 +0200

    Added signing support

--- AbsEcoTool/EcoSupport.cs
+++ AbsEcoTool/EcoSupport.cs
@@ -3,10 +3,27 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
-namespace AbstractionBuilder
+namespace AbsEcoTool
 {
     public static class EcoSupport
     {
+        public static void SignAbstractionWithKey(string abstractionFolder, string certDir, string keyId)
+        {
+            string abstractionID = getAbstractionID(abstractionFolder);
+            executeGetSignContent(certDir, abstractionFolder);
+            signContentWithKey(certDir, abstractionID, keyId);
+        }
+
+        private static void signContentWithKey(string certDir, string abstractionId, string keyId)
+        {
+            CmdSupport.ExecuteScriptWithOutput("signXwithKeyY.cmd " + abstractionId + " " + keyId, certDir);
+        }
+
+        private static void executeGetSignContent(string certDir, string abstractionFolder)
+        {
+            CmdSupport.ExecuteScriptWithOutput("gitgetcommitdata.cmd ..\\" + abstractionFolder, certDir);
+        }
+
         public static void AddToDirectory(string abstractionFolder, string directoryFolder)
         {
             string abstractionID = getAbstractionID(abstractionFolder);
@@ -23,17 +40,19 @@ namespace AbstractionBuilder
 
         private static string getFullPathName(string directoryFolder, string abstractionId)
         {
-            throw new NotImplementedException();
+            return Path.Combine(directoryFolder, abstractionId);
         }
 
         private static string createDirectoryContent(string abstractionId, string[] urlLocations)
         {
-            throw new NotImplementedException();
+            return String.Join("\n", urlLocations);
         }
 
         private static string[] getUrlLocations(string abstractionFolder)
         {
-            string[] remotes = CmdSupport.ExecuteWithOutput("git", "remote -v", abstractionFolder);
+            string[] remotes = CmdSupport.ExecuteGitWithOutput("remote -v", abstractionFolder);
+            if(remotes.Length == 0)
+                throw new NotSupportedException("Git remotes are empty: " + abstractionFolder);
             remotes = remotes.Where(remote => remote.Contains("(fetch)")).Select(remote =>
                                                                                      {
                                                                                          string[] split =
@@ -49,13 +68,24 @@ namespace AbstractionBuilder
         private static string getAbstractionID(string abstractionFolder)
         {
             string abstractionID =
-                CmdSupport.ExecuteWithOutput("git", "log --format=%H -n 1", abstractionFolder).First();
+                CmdSupport.ExecuteGitWithOutput("log --format=%H -n 1", abstractionFolder).First();
             return abstractionID;
         }
     }
 
     internal static class CmdSupport
     {
+
+        public static string[] ExecuteScriptWithOutput(string arguments, string workingDirectory)
+        {
+            return ExecuteWithOutput("cmd", "/c " + arguments, workingDirectory);
+        }
+
+        public static string[] ExecuteGitWithOutput(string arguments, string workingDirectory)
+        {
+            return ExecuteWithOutput("cmd", "/c git " + arguments, workingDirectory);
+        }
+
         public static string[] ExecuteWithOutput(string command, string arguments, string workingDirectory)
         {
             ProcessStartInfo startInfo = new ProcessStartInfo();
@@ -63,11 +93,15 @@ namespace AbstractionBuilder
             startInfo.WorkingDirectory = workingDirectory;
             startInfo.FileName = command;
             startInfo.Arguments = arguments;
+            startInfo.UseShellExecute = false;
             startInfo.RedirectStandardOutput = true;
-            Process proc = Process.Start(startInfo);
+            Process proc = new Process();
+            proc.StartInfo = startInfo;
+            proc.Start();
             proc.WaitForExit();
             string stdOutput = proc.StandardOutput.ReadToEnd();
-            return stdOutput.Split('\n');
+            return stdOutput.Split(new char[] {'\n'}, StringSplitOptions.RemoveEmptyEntries);
         }
+
     }
 }
--- AbsEcoTool/Program.cs
+++ AbsEcoTool/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using CommandLine.Utility;
 
 namespace AbsEcoTool
 {
@@ -9,6 +11,46 @@ namespace AbsEcoTool
     {
         static void Main(string[] args)
         {
+            Arguments arguments = new Arguments(args);
+            if(args == null || args.Length < 2)
+            {
+                printUsage();
+                return;
+            }
+            //Debugger.Launch();
+            string command = args[0];
+            switch (command)
+            {
+                case "add":
+                    {
+                        string absPath = arguments.RequireParameter("abspath");
+                        string dirPath = arguments.RequireParameter("dirpath");
+                        EcoSupport.AddToDirectory(absPath, dirPath);
+                        break;
+                    }
+                case "sign":
+                    {
+                        string absPath = arguments.RequireParameter("abspath");
+                        string certDir = arguments.RequireParameter("certpath");
+                        string keyId = arguments.RequireParameter("key");
+                        EcoSupport.SignAbstractionWithKey(absPath, certDir, keyId);
+                        break;
+                    }
+                default:
+                    printUsage();
+                    break;
+            }
+        }
+
+        private static void printUsage()
+        {
+            Console.WriteLine("Usage: AbsEcoTool add -abspath=<absdirectory> -dirpath=<directorydirectory>");
+            Console.WriteLine("Usage: AbsEcoTool sign -abspath=<absdirectory> -certpath=<certdirectory> -key=<keyid>");
         }
     }
 }
+
+namespace CommandLine.Utility
+{
+}
+
